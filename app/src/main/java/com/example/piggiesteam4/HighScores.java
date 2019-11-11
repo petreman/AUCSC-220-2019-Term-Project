17675d77da7aad3a780313020e7274877a0dcda2
Alvin <56981984+alvinjw@users.noreply.github.com>
2019-11-11 15:23:29 -0700

    Added getters/setters for Scores. Implements Comparable for sorting

--- app/src/main/java/com/example/piggiesteam4/HighScores.java
+++ app/src/main/java/com/example/piggiesteam4/HighScores.java
@@ -2,7 +2,7 @@ package com.example.piggiesteam4;
 
 public class HighScores {
 
-    private static class Score {
+    private static class Score implements Comparable{
         private String name;
         private int score;
 
@@ -14,5 +14,47 @@ public class HighScores {
             this.score = score;
         }//constructor
 
+        /**
+         * Gets the player's name.
+         * @return the player's name
+         */
+        public String getName() {
+            return name;
+        }//getName
+
+        /**
+         * Sets the player's name.
+         * @param name the player's name.
+         */
+        public void setName(String name) {
+            this.name = name;
+        }//setName
+
+        /**
+         * Gets the player's score.
+         * @return the player's score.
+         */
+        public int getScore() {
+            return score;
+        }//getScore
+
+        /**
+         * Sets the player's score.
+         * @param score the player's score.
+         */
+        public void setScore(int score) {
+            this.score = score;
+        }
+
+        /**
+         * Compares this Score to another Score.
+         * @param otherPlayer
+         * @return whether greater, equal, or lesser.
+         */
+        @Override
+        public int compareTo(Object otherPlayer) {
+            int otherScore = ((Score)otherPlayer).getScore();
+            return otherScore - this.score;
+        }//compareTo
     }//Score
 }//HighScore
