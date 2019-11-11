5f21dac93083943ed21c59c20960480f75dcb0f8
Alvin <56981984+alvinjw@users.noreply.github.com>
2019-11-11 15:16:19 -0700

    HighScores, Score classes created, score constructor added

---
+++ app/src/main/java/com/example/piggiesteam4/HighScores.java
@@ -0,0 +1,18 @@
+package com.example.piggiesteam4;
+
+public class HighScores {
+
+    private static class Score {
+        private String name;
+        private int score;
+
+        /**
+         * Constructor.
+         */
+        Score(String name, int score){
+            this.name = name;
+            this.score = score;
+        }//constructor
+
+    }//Score
+}//HighScore
