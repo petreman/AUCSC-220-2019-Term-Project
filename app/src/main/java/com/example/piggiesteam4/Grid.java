e9fa19aff9080e9c9b1680dca013d8e4a1c9b3f9
Keegan Petreman <keeganp218@hotmail.com>
2019-11-19 13:04:15 -0700

    Added functions to check for pens made by horizontal fences

--- app/src/main/java/com/example/piggiesteam4/Grid.java
+++ app/src/main/java/com/example/piggiesteam4/Grid.java
@@ -80,7 +80,8 @@ public class Grid {
     }//clearGrid
 
     /**
-     *
+     * Tries to place a horizontal fence at the specified grid location
+     * Need to index into xCoords to get correct location
      *
      * @param x - row coord into xCoords
      * @param y - column coord into xCoords
@@ -97,9 +98,83 @@ public class Grid {
             return true;
         }//if
 
+        else {
+            return false;
+        }//else
+
+    }//setFenceX
+
+    /**
+     * Tries to place a vertical fence at the specified grid location in game class
+     * Need to index into yCoords to get correct location
+     *
+     * @param x - row coord into xCoords
+     * @param y - column coord into xCoords
+     * @param color - color to set the fence to
+     * @return - true if placed successfully,
+     *           false if fence already exits at that spot
+     * TODO add player as a parameter!
+     */
+    boolean setFenceY(int x, int y, int color){
+
+        if (this.yCoords[x][y].exists() == false){
+            this.yCoords[x][y].setExistence(true);
+            //this.yCoords[x][y].setColor(color);
+            return true;
+        }//if
+
+        else {
+            return false;
+        }//else
+
+    }//setFenceY
+
+    /**
+     * Checks for a completed pen starting by checking the horizontal fences.
+     * Intended to called after successfully placing a horizontal fence.
+     * For vertical fences, it's safest to use completedPenY.
+     *
+     * The fence at the coordinates into xCoords are assumed to be for a fence that exists
+     *
+     * @param xIn - row coord into xCoords
+     * @param yIn - column coord into xCoords
+     * @return - true if a pen is completed, false otherwise
+     */
+    boolean checkPenBelow(int xIn, int yIn){
+
+        if (this.xCoords[xIn+1][yIn].exists() && this.yCoords[xIn][yIn].exists() &&
+                this.yCoords[xIn][yIn + 1].exists()){
+            return true;
+        }//if
+
         else{
             return false;
+        }//else
 
-    }//boolean
+    }//checkPenBelow
+
+    /**
+     * Checks for a completed pen starting by checking the horizontal fences.
+     * Intended to called after successfully placing a horizontal fence.
+     * For vertical fences, it's safest to use completedPenY.
+     *
+     * The fence at the coordinates into xCoords are assumed to be for a fence that exists
+     *
+     * @param xIn - row coord into xCoords
+     * @param yIn - column coord into xCoords
+     * @return - true if a pen is completed, false otherwise
+     */
+    boolean checkPenAbove(int xIn, int yIn){
+
+        if (this.xCoords[xIn - 1][yIn].exists() && this.yCoords[xIn - 1][yIn].exists() &&
+                this.yCoords[xIn - 1][yIn + 1].exists()){
+            return true;
+        }//if
+
+        else{
+            return false;
+        }//else
+
+    }//checkPenAbove
 
 }//grid
