299ef10ef5ed66d729defedbd743e2211c44ca75
ArnoldG <gihozo@ualberta.ca>
2019-11-19 19:58:40 -0700

    added score and changed color to int

--- app/src/main/java/com/example/piggiesteam4/Player.java
+++
@@ -1,7 +0,0 @@
-package com.example.piggiesteam4;
-
-public class Player {
-
-    int uu = 1 + 1;
-    int o = 4 +2;
-}
---
+++ app/src/main/java/com/example/piggiesteam4/TestBoardInitialization.java
@@ -0,0 +1,5 @@
+package com.example.piggiesteam4;
+
+public class TestBoardInitialization {
+
+}
