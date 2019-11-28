153ecadc0ea3a74703c68583c2bfc127697abe52
Keegan Petreman <keeganp218@hotmail.com>
2019-11-27 19:53:34 -0700

    Inital reimplementation of visible grid setup: the original system wasn't very robust or reusable and would cause headaches later

--- app/src/main/java/com/example/piggiesteam4/GridFragment.java
+++ app/src/main/java/com/example/piggiesteam4/GridFragment.java
@@ -67,7 +67,7 @@ public class GridFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_grid_55, container, false);
+        return inflater.inflate(R.layout.fragment_grid_66, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
